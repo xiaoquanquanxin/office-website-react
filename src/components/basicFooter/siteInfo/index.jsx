8eaf136dbeee4106db9ac147a7d3ded9aaab5a65
quanxin <quanxin@pujitech.com>
2021-08-05 15:40:52 +0800

    footer

--- src/components/basicFooter/siteInfo/index.jsx
+++ src/components/basicFooter/siteInfo/index.jsx
@@ -4,6 +4,7 @@ import layout from '@css/layout.module.less';
 import { stopPropagation } from '@utils/eventListener';
 import { connect } from 'react-redux';
 import { mapDispatchToProps, mapStateToProps } from '@store/reduxMap';
+import putOnRecords from '@media/basicFooter/putOnRecords.png';
 
 export const SiteInfo = connect(
     mapStateToProps,
@@ -17,11 +18,19 @@ export const SiteInfo = connect(
         //
         data,
     }) => {
-        if (!data) {
-            return '';
+        if (!data || !data.data) {
+            return null;
         }
         //  浏览器足够宽
         const { isRelativeWide } = REDUCER_BROWSER_INFO;
+        const {copy_right} = data.data;
+        const copy_right_list = (copy_right || '').split(';');
+        const text = copy_right_list[0];
+        const href1 = copy_right_list[1];
+        const linkText1 = copy_right_list[2];
+        const href2 = copy_right_list[3];
+        const linkText2 = copy_right_list[4];
+
         return (
             <div className={style.siteInfo}>
                 <div className={`${style.siteInfoInner} ${layout.clearfix}`}>
@@ -48,8 +57,16 @@ export const SiteInfo = connect(
                                 className={`${style.qrCode} ${qrCodeShowIndex === 1 ? layout.block : layout.none}`} />
                         </button>
                     </div>
-                    <div className={`${style.siteInfoMsg} ${isRelativeWide ? layout.left : ''} `}
-                        dangerouslySetInnerHTML={{ __html: data.data.copy_right }} />
+                    <div className={`${style.siteInfoMsg} ${isRelativeWide ? layout.left : ''} `}>
+                        {text}
+                        <a className={style.linkHref} rel="noopener noreferrer" target="_blank" href={href1}
+                           dangerouslySetInnerHTML={{__html: linkText1}}
+                        />
+                        <img className={style.linkImg} src={putOnRecords} alt=''/>
+                        <a className={style.linkHref} rel="noopener noreferrer" target="_blank" href={href2}
+                           dangerouslySetInnerHTML={{__html: linkText2}}
+                        />
+                    </div>
                 </div>
             </div>
         );
